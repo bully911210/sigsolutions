8ad62af8a85f478f8bf369b6d67cd0a331076a09
badenhorst.franz <franz@sigsolutions.co.za>
2025-04-14 14:00:52 +0200

    v0

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { checkWebsiteLoaded, setPageTitle } from './utils'
+
+// Check if the website is loaded properly
+if (checkWebsiteLoaded()) {
+  console.log('SIG Solutions website loaded successfully')
+  setPageTitle('Home')
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
