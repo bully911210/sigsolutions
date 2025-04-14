a13f70c80ec26f95a14198a3c4b3261be063877d
badenhorst.franz <franz@sigsolutions.co.za>
2025-04-14 14:01:03 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <h1>SIG Solutions</h1>
       <p>Welcome to our website!</p>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
