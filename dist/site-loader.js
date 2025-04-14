5c57ca59693b2545ce3be710640f933f4249b8f2
badenhorst.franz <franz@sigsolutions.co.za>
2025-04-14 13:36:50 +0200

    v0

--- dist/site-loader.js
+++ dist/site-loader.js
@@ -1,32 +1,32 @@
 // This script checks if the main site is working and redirects to the non-module version if it's not
 (function() {
   console.log('Site loader script running');
-  
+
   // Function to check if a script can be loaded
   function checkScriptLoading(url, timeout) {
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
       let timer;
-      
+
       script.type = 'module';
       script.src = url;
-      
+
       script.onload = () => {
         clearTimeout(timer);
         resolve(true);
       };
-      
+
       script.onerror = () => {
         clearTimeout(timer);
         resolve(false);
       };
-      
+
       timer = setTimeout(() => {
         resolve(false);
       }, timeout);
-      
+
       document.head.appendChild(script);
-      
+
       // Remove the script after checking
       setTimeout(() => {
         if (script.parentNode) {
@@ -35,21 +35,21 @@
       }, timeout + 100);
     });
   }
-  
+
   // Check if we're already on the non-module version
   if (window.location.pathname.includes('index-nomodule.html')) {
     console.log('Already on non-module version');
     return;
   }
-  
+
   // Check if ES modules are working
   async function checkAndRedirect() {
     try {
       console.log('Checking if ES modules are working...');
-      
+
       // Try to load a test module
-      const moduleWorks = await checkScriptLoading('./assets/vendor-C3d4I53_.js', 5000);
-      
+      const moduleWorks = await checkScriptLoading('./assets/vendor-C3d4I53_.mjs', 5000);
+
       if (!moduleWorks) {
         console.log('ES modules not working, redirecting to non-module version');
         window.location.href = './index-nomodule.html';
@@ -61,7 +61,7 @@
       window.location.href = './index-nomodule.html';
     }
   }
-  
+
   // Run the check
   checkAndRedirect();
 })();
